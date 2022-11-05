3cc707e2bcfe9110d34123bfca5b4004a27ce516
OvOlzhas <turlybekov.o@phystech.edu>
2022-11-05 03:17:26 +0300

    Add LevelGenerator(alpha)

---
+++ Assets/Code/Room.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Vector2Int Size = new Vector2Int(10, 10);
+    
+    void Start()
+    {
+    }
+}
