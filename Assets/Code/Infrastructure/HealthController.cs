b7314d453159a14772d0486212119ab96cf6f89b
Kirill Piatkovskiy <piatkovskii.k@phystech.edu>
2022-12-14 14:54:37 +0300

    Added controller

---
+++ Assets/Code/Infrastructure/HealthController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class HealthController : MonoBehaviour
+{
+    // Fields --------------------------------------------
+    public float healPointsMax = 5f;
+    public float healPointsMin = 0f;
+    float healPointsCur;
+    // ---------------------------------------------------
+
+
+
+    // Properties ----------------------------------------
+
+    // ---------------------------------------------------
+
+
+
+    // Events --------------------------------------------
+
+    // ---------------------------------------------------
+
+
+
+    // Injection -----------------------------------------
+    [Inject]
+    private void Constructor()
+    {
+
+    }
+    // ---------------------------------------------------
+
+
+
+    // Unity Methods -------------------------------------
+    protected void Start()
+    {
+
+    }
+
+    protected void Update()
+    {
+
+    }
+    // ---------------------------------------------------
+
+
+
+    // Other Methods -------------------------------------
+    public void IncreaseHealPoints(float value)
+    {
+
+        healPointsCur = Mathf.Clamp(healPointsCur + value, 0, healPointsMax);
+        //healIndicator.SetValue(healPointsCur);
+
+    }
+    public void DecreaseHealPoints()
+    {
+        --healPointsCur;
+        //healIndicator.SetValue(healPointsCur);
+        if (healPointsCur <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // ---------------------------------------------------
+}
